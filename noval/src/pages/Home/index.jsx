f5ee49e520c15457c34ce7df0aa21236ea3723f2
yuanfelin <3516955776@qq.com>
2025-08-04 10:35:22 +0800

    首页布局

--- noval/src/pages/Home/index.jsx
+++ noval/src/pages/Home/index.jsx
@@ -1,14 +1,35 @@
-import useTitle  from '@/hooks/useTitle'
+import { Search } from 'react-vant'
+import useTitle from '../../hooks/useTitle'
+import styles from './home.module.css'
+import { useNavigate } from 'react-router-dom'
+import { AppsO } from '@react-vant/icons'
+import { useBookStore } from '../../store/useBookStore'
+import { useEffect } from 'react'
+import Waterfall from '../../components/Waterfall'
 
-const BookShelf =()=>{
-    useTitle('诸天小说-书架')
-    return(
-       
-        <>
-        这是 BookShelf 页面
-        
-        </>
+const Home = () => {
+    const navigate = useNavigate()
+    const { loading, books, fetchMore } = useBookStore();
+    
+    useTitle('诸天小说-首页')
+    
+    useEffect(() => {
+        fetchMore()
+    }, [fetchMore])
+    
+    return (
+        <div>
+            <Search placeholder="请输入搜索内容" onClickInput={()=>{navigate('/search')}} action={
+                <AppsO onClick={
+                    () => navigate('/sort')} fontSize={30} style={{ marginLeft: '10px' }
+                    } />
+            } />
+
+            <div className={styles.AI_ball} onClick={() => navigate('/ai_chat')}>AI小助手</div>
+
+            <Waterfall books={books} fetchMore={fetchMore} loading={loading}/>
+        </div>
     )
 }
 
-export default BookShelf
+export default Home
