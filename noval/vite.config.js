5c4ae08851a4d5f71660270b697268224df5ff1e
selencestar <3516955776@qq.com>
2025-09-13 15:11:11 +0800

    项目架构及环境配置

--- noval/vite.config.js
+++ noval/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteMockServe } from "vite-plugin-mock";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteMockServe({
+      mockPath: "mock",
+      localEnabled: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
