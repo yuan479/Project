c8c1801f7439fde9c67e86d049372d40227a4a3b
selencestar <3516955776@qq.com>
2025-09-20 23:56:24 +0800

    拖拽效果实现

--- lownode_editor/src/editor/components/Material/index.tsx
+++ lownode_editor/src/editor/components/Material/index.tsx
@@ -1,5 +1,6 @@
 import {useMemo} from 'react'
 import {useComponentConfigStore} from '../../stores/component-config'
+import {MaterialItem} from '../MaterialItem'
 
 export function Material() {
     const {componentConfig} = useComponentConfigStore()
@@ -10,13 +11,8 @@ export function Material() {
         <div>
            {
             components.map(item=>{
-                return(
-                    <div
-                    className='border-dashed border-[1px] border-[#000] py-[8px] px-[10px] m-[10px] cursor-move inline-block bg-white hover:bg-[#ccc]'
-                    >
-                        {item.name}
-                    </div>
-                )
+                return<MaterialItem key={item.name} name={item.name} />
+                
             })
            }
         </div>
---
+++ lownode_editor/src/editor/components/MaterialItem/index.tsx
@@ -0,0 +1,26 @@
+import {useDrag} from 'react-dnd'
+
+export interface MaterialItemProps {
+    name: string;
+}
+
+export function MaterialItem(props: MaterialItemProps) {
+
+    const {name} = props
+    const[_,drag] = useDrag({
+        type:name,
+        // 数据项
+        item:{
+            type:name
+        }
+    })
+
+    return (
+        <div
+        ref={drag}
+        className='border-dashed border-[1px] border-[#000] py-[8px] px-[10px] m-[10px] cursor-move inline-block bg-white hover:bg-[#ccc]'
+        >
+            {name}
+        </div>
+    )
+}
